36dbd2ccef232038d7254d883219736b3ed6ecc3
Robin Bauwens <Robin Bauwens@192.168.0.241>
2016-02-09 20:37:08 +0100

    modificatieTest

---
+++ src/testKlasse.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Robin Bauwens
+ */
+public class testKlasse {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+
+            System.out.printf("%d \t", i);
+
+        }
+
+    }
+}
